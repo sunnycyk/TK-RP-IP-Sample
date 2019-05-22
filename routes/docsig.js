b1acb1e1dc75fafd3a90104cec0df018b9e02ef0
Sunny Cheung <sunnycyk@yahoo.com>
2019-05-23 01:30:18 +0800

    decorate docsig status page (#18)
    * decorate docsig status page
    
    * add docsig Error page
    
    * fix Path.join

--- routes/docsig.js
+++ routes/docsig.js
@@ -4,6 +4,7 @@ const DocSig = require('../util/tkdocsig')
 const Cache = require('../util/cache')
 const UUID = require('uuid')
 const Config = require('../config')
+const Path = require('path')
 const FS = require('fs')
 
 const Host = Config.host
@@ -95,12 +96,12 @@ Router.get('/docsig/status', async(_, res) => {
       setStatus(defaultStatus)
       res.set('Content-Type', 'application/pdf').send(dataBuff)
     } else {
-      res.set('Refresh', '5;url=/docsig/status').json(status)
+      res.sendFile(Path.join(__dirname, '/../docsigStatus.html'))
     }
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error('DocSig Status Error: ', e)
-    res.status(500).json({err: e.message})
+    res.sendFile(Path.join(__dirname, '../docsigError.html'))
   }
 })
 
